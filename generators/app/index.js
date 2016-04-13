c8399b5c0c7b7fe43676e7a67f56412943f8c4e5
luke <luke.edwards05@gmail.com>
2016-04-13 08:35:30 -0700

    add prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,5 +1,6 @@
 'use strict';
 var yeoman = require('yeoman-generator');
+var formatUrl = require('normalize-url');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
@@ -10,19 +11,77 @@ module.exports = yeoman.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the sublime ' + chalk.red('generator-riot-tag') + ' generator!'
+      'Welcome to the ' + chalk.red('generator-riot-tag') + ' generator!'
     ));
 
     var prompts = [{
+      name: 'uName',
+      message: 'What\'s your name?',
+      store: true,
+      validate: function(val) {
+        return val.trim().length > 0 ? true : 'name is required';
+      }
+    }, {
+      name: 'uGithub',
+      message: 'What\'s your GitHub username?',
+      store: true,
+      validate: function(val) {
+        return val.trim().length > 0 ? true : 'github username is required';
+      }
+    }, {
+      name: 'uWebsite',
+      message: 'What\'s your website?',
+      store: true,
+      default: function(props) {
+        return 'https://github.com/' + props.uGithub;
+      }
+    }, {
+      type: 'list',
+      name: 'tabType',
+      message: 'Do you use tabs or spaces?',
+      choices: ['tab', 'space'],
+      store: true
+    }, {
+      type: 'list',
+      name: 'tabWidth',
+      message: 'How about spacing width?',
+      choices: ['2', '4'],
+      store: true
+    }, {
       type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
+      name: 'useColors',
+      message: 'Include Material Design color palette? (SASS)',
+      default: true,
+      store: true
+    }, {
+      type: 'confirm',
+      name: 'useBower',
+      message: 'Publish to the Bower registry?',
+      default: true,
+      store: true
+    }, {
+      name: 'tagName',
+      message: 'What\'s the name of your Riot tag?',
+      store: true,
+      default: function(props) {
+        return 'my-tag';
+      },
+      validate: function(val) {
+        return val.trim().length > 0 ? true : 'tag name is required';
+      }
+    }, {
+      type: 'confirm',
+      name: 'gitinit',
+      message: 'Initialize a Git repository?',
+      store: true,
       default: true
     }];
 
     this.prompt(prompts, function (props) {
       this.props = props;
-      // To access props later use this.props.someAnswer;
+
+      this.props.uWebsite = formatUrl(props.uWebsite);
+      this.props.tagName = props.tagName.replace(' ', '-');
 
       done();
     }.bind(this));
